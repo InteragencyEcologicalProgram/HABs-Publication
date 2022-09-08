3ff95b78ad033603e322299595f979168a4fa71e
keithbg <kbouma-gregson@usgs.gov>
2022-09-08 13:21:40 -0700

    add region only model

--- MicrocystisRatingModels_KBG/Scripts/MCindex_models.R
+++ MicrocystisRatingModels_KBG/Scripts/MCindex_models.R
@@ -133,6 +133,17 @@ fit_max_mc_SI.acat <- brm(
   backend = "cmdstanr")
 #control = list(adapt_delta = 0.99)
 
+fit_max_mc_SI.region.cumu <- brm(
+  formula = mc_max ~ 1 + cs(Yr_type) + Region + (1|Station),
+  data = HABs.stats,
+  family = cumulative("probit"),
+  chains= 4,
+  iter= 5000,
+  warmup= 1000,
+  cores= 4,
+  backend = "cmdstanr")
+#control = list(adapt_delta = 0.99)
+
 
 
 #save(fit_max_mc_SI.1, file= "Data/fit_max_mc1b.Rdata")
@@ -143,6 +154,14 @@ fit_max_mc_SI.acat <- brm(
 ## Extract marginal effects
 max_mc1_conditions.cumu <- make_conditions(fit_max_mc_SI.cumu, c("Region", "MonthF"))
 max_mc1_effects.cumu <- conditional_effects(fit_max_mc_SI.cumu, "Yr_type", condition= max_mc1_conditions.cumu, categorical= TRUE)$`Yr_type` %>% 
+  mutate(Yr_type= factor(Yr_type, ordered= TRUE, levels= c("Critical", "Dry", "Below Normal", "Wet")))
+
+max_mc2_conditions.cumu <- make_conditions(fit_max_mc_SI.cumu, c("Region"))
+max_mc2_effects.cumu <- conditional_effects(fit_max_mc_SI.cumu, "Yr_type", condition= max_mc2_conditions.cumu, categorical= TRUE)$`Yr_type` %>% 
+  mutate(Yr_type= factor(Yr_type, ordered= TRUE, levels= c("Critical", "Dry", "Below Normal", "Wet")))
+
+max_mc1_conditions.region.cumu <- make_conditions(fit_max_mc_SI.region.cumu, c("Region"))
+max_mc1_effects.region.cumu <- conditional_effects(fit_max_mc_SI.region.cumu, "Yr_type", condition= max_mc1_conditions.region.cumu, categorical= TRUE)$`Yr_type` %>% 
   mutate(Yr_type= factor(Yr_type, ordered= TRUE, levels= c("Critical", "Dry", "Below Normal", "Wet")))
 
 
@@ -163,6 +182,15 @@ ggplot(max_mc1_effects.cumu, aes(x= cats__, y= estimate__, group= Yr_type)) +
   facet_rep_grid(Region ~ MonthF, repeat.tick.labels = TRUE) 
   #ggsave(last_plot(), filename= "MCrating_probs_LowHigh_Season.png", width= 6.5, height= 8.5, dpi= 300,
 #       path= "Figures")
+
+ggplot(max_mc1_effects.region.cumu, aes(x= cats__, y= estimate__, group= Yr_type)) +
+  geom_col(aes(fill= Yr_type), color= "black", position= position_dodge()) +
+  geom_errorbar(aes(ymin= lower__, ymax= upper__), width= 0.5, position= position_dodge(0.9)) +
+  scale_fill_manual(values= c("firebrick", "darkorange2", "lightgoldenrod", "lightskyblue"), 
+                    name= "Water Year") +
+  labs(x= expression(paste(italic("Microcystis"), " Rating Level")), y= "Probability") +
+  scale_y_continuous(expand= c(0, 0), limits= c(0, 1)) +
+  facet_rep_grid(~ Region, repeat.tick.labels = TRUE) 
 
 
 ggplot(max_mc1_effects2.acat, aes(x= cats__, y= estimate__, group= Yr_type)) +
